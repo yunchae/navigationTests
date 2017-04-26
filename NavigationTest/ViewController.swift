ff82cfaa4e828a82585dbe587aef8d10a1a6b130
SDS-019 <sds-019@70-12-244-200.pools.spcsdns.net>
2017-04-26 13:33:13 +0900

    Initial Commit

---
+++ NavigationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationTest
+//
+//  Created by SDS-019 on 2017. 4. 26..
+//  Copyright © 2017년 SDS-019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
